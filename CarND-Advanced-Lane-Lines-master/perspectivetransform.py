6680cd045697c287527b0d7ab323303a4efd85a5
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-03-25 00:11:25 +0100

    small tunning here and there

--- CarND-Advanced-Lane-Lines-master/perspectivetransform.py
+++ CarND-Advanced-Lane-Lines-master/perspectivetransform.py
@@ -5,28 +5,56 @@ import cv2
 
 
 def perspective_pipeline():
+    """
+    this function iterates thru all the images and applies birds' view transformation
+    :return: it doesn't return anything if everything is fine
+    """
     files_to_transform = glob("output_images\\lines_*.jpg")
     for file in files_to_transform:
-        perspective_transform(file, 'output_images\\warped_lines_'+file.split('\\')[-1])
+        warped, unwarped, _, _ = \
+            perspective_transform(file)
+        cv2.imwrite('output_images\\warped_'+file.split('\\')[-1], warped)
+        cv2.imwrite('output_images\\unwarped_'+file.split('\\')[-1], unwarped)
     return
 
 
-def perspective_transform(src_file, dst_file):
+def perspective_transform(src_file):
     """
     this function transforms images to birds' view
     :param src_file: file to transform
-    :param dst_file: file name of the file under which the results should be stored
-    :return: this function doesn't return anything
+    :return: warped, unwarped images, matrices: M and MInv
     """
+
     img = cv2.imread(src_file)  # Read the test img
 
-    (IMAGE_H, IMAGE_W) = (img.shape[1], img.shape[0])
-    src = np.float32([[0, IMAGE_H], [1207, IMAGE_H], [0, 0], [IMAGE_W, 0]])
-    dst = np.float32([[569, IMAGE_H], [711, IMAGE_H], [0, 0], [IMAGE_W, 0]])
-    M = cv2.getPerspectiveTransform(src, dst)  # The transformation matrix
-    Minv = cv2.getPerspectiveTransform(dst, src)  # Inverse transformation
+    perspective_delta_x = img.shape[0]
+    perspective_delta_y = int(perspective_delta_x * 30 / 3.7)
+    perspective_border_x = int(perspective_delta_x * 0.7)
+    perspective_max_y = perspective_delta_y
+    perspective_max_x = int(perspective_delta_x + 2 * perspective_border_x)
 
-    img = img[450:(450 + IMAGE_H), 0:IMAGE_W]  # Apply np slicing for ROI crop
-    warped_img = cv2.warpPerspective(img, M, (IMAGE_W, IMAGE_H))  # Image warping
-    # warped = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
-    cv2.imwrite(dst_file, warped_img)
+    perspective_origin_y_top = 440
+    perspective_origin_y_bottom = 670
+    perspective_origin_x_top_left = 609
+    perspective_origin_x_top_right = 673
+    perspective_origin_x_bottom_left = 289
+    perspective_origin_x_bottom_right = 1032
+
+    src = np.float32(
+        [[perspective_origin_x_top_left, perspective_origin_y_top],
+         [perspective_origin_x_top_right, perspective_origin_y_top],
+         [perspective_origin_x_bottom_left, perspective_origin_y_bottom],
+         [perspective_origin_x_bottom_right, perspective_origin_y_bottom]])
+
+    dst = np.float32(
+        [[perspective_border_x, 0],
+         [perspective_border_x + perspective_delta_x, 0],
+         [perspective_border_x, perspective_delta_y],
+         [perspective_border_x + perspective_delta_x, perspective_delta_y]])
+
+    m = cv2.getPerspectiveTransform(src, dst)  # The transformation matrix
+    minv = cv2.getPerspectiveTransform(dst, src)  # Inverse transformation
+    warped_img = cv2.warpPerspective(img, m, (perspective_max_x, perspective_max_y), flags=cv2.INTER_LINEAR)
+    unwarped_img = \
+        cv2.warpPerspective(warped_img, minv, (img.shape[0], img.shape[1]), flags=cv2.INTER_LINEAR)
+    return warped_img, unwarped_img, m, minv
