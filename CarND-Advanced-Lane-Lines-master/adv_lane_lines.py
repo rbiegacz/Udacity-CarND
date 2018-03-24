14e5357344cd454c1ae5c073986ff1b81a16726d
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-03-25 00:10:35 +0100

    draw_lane function added

--- CarND-Advanced-Lane-Lines-master/adv_lane_lines.py
+++ CarND-Advanced-Lane-Lines-master/adv_lane_lines.py
@@ -172,6 +172,26 @@ def determine_lane_curvature():
     # Example values: 632.1 m    626.2 m
 
 
+def draw_lane():
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (image.shape[1], image.shape[0]))
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    plt.imshow(result)
+
+
 def main():
     """
     This function implements the main flow of the application
