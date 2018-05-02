819f1ef9e898cc932920a1aa1158f04417f0e959
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-05-02 22:07:04 +0200

    small update

--- CarND-Vehicle-Detection-master/object_detection_colorspaces.py
+++ CarND-Vehicle-Detection-master/object_detection_colorspaces.py
@@ -1,24 +1,26 @@
 """
-This module shows how to use different color spaces to identify clusters of pixels;
-Clusters of pixels are useful while discoverying objects like sky, road, vehicle, etc.
+    This module shows how to use different color spaces to identify clusters of pixels;
+    Clusters of pixels are useful while discoverying objects like sky, road, vehicle, etc.
 
-Use this to first explore some video frames, and see if you can locate clusters of colors
-that correspond to the sky, trees, specific cars, etc.
-Here are some sample images for you to use
+    Use this to first explore some video frames, and see if you can locate clusters of colors
+    that correspond to the sky, trees, specific cars, etc.
+    Here are some sample images for you to use
 
-A lot of code included in this file comes from Udacity Self-Driving Car Engineer Nanodegree
+    A lot of code included in this file comes from Udacity Self-Driving Car Engineer Nanodegree
 """
 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import object_detection_utils
 
 AXIS_LIMITS = ((0, 255), (0, 255), (0, 255))
 
 
 def plot3d(pixels, colors_rgb, axis_labels=list("RGB"), axis_limits=AXIS_LIMITS):
     """
+    TODO: finish description of this function
     Plot pixels in 3D.
     :param pixels:
     :param colors_rgb:
@@ -26,7 +28,6 @@ def plot3d(pixels, colors_rgb, axis_labels=list("RGB"), axis_limits=AXIS_LIMITS)
     :param axis_limits:
     :return:
     """
-
     # Create figure and 3D axes
     fig = plt.figure(figsize=(8, 8))
     ax = Axes3D(fig)
