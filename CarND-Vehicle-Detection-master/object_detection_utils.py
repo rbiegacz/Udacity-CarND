31f27544621e70a9c3beae672a4b5a61b1c98503
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-05-02 22:17:47 +0200

    update related to code refactoring

--- CarND-Vehicle-Detection-master/object_detection_utils.py
+++ CarND-Vehicle-Detection-master/object_detection_utils.py
@@ -1,21 +1,22 @@
 """
-    this module contains utility functions for object detection
+    This module contains utility functions for object detection
+    The code included in this file comes from Udacity Self-Driving Car Engineer Nanodegree
 """
 import numpy as np
+import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import cv2
+from skimage.feature import hog
 
 
-# Here is your draw_boxes function from the previous exercise
 def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
     """
-    this function draws boxes
+    This function draws rectangles specified by bbox in the picture img.
     :param img: image where the boxes should be drawn
     :param bboxes: the list of boxes to draw
     :param color: color of a box
     :param thick: thickness of a line used to draw boxes
-    :return:
+    :return: it returnes modified image which is original images + rectangles
     """
     imcopy = np.copy(img)
     # Iterate through the bounding boxes
@@ -24,6 +25,7 @@ def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
     # Return the image copy with boxes drawn
     return imcopy
+
 
 def util_color_hist(img, bins_number=32, bins_range=(0, 256)):
     """
@@ -44,6 +46,210 @@ def util_color_hist(img, bins_number=32, bins_range=(0, 256)):
     hist_features = np.concatenate((rhist[0], ghist[0], bhist[0]))
     # Return the individual histograms, bin_centers and feature vector
     return rhist, ghist, bhist, bin_centers, hist_features
+
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block,
+                     vis=False, feature_vec=True):
+    """
+    This function retrieves HOG features for a given image.
+    More on Histogram of Oriented Gradients could be read here:
+    http://scikit-image.org/docs/dev/auto_examples/features_detection/plot_hog.html
+    This function uses hog(...) function described here:
+    http://scikit-image.org/docs/dev/api/skimage.feature.html#skimage.feature.hog
+    :param img: image to analize
+    :param orient: number of orientation bins
+    :param pix_per_cell: size in pixels of a cell
+    :param cell_per_block: number of cells in each block
+    :param vis: if true then return an image of HOG; For each cell and orientation bin, the image contains a line segment
+    (more could be read here: http://scikit-image.org/docs/dev/api/skimage.feature.html#skimage.feature.hog)
+    :return: HOG description of the image; if feature_vec is True then flattened array is returned
+    """
+    # Call with two outputs if vis==True
+    if vis is True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), block_norm='L2-Hys',
+                                  transform_sqrt=True,
+                                  visualise=vis, feature_vector=feature_vec)
+        return features, hog_image
+    # Otherwise call with one output
+    else:
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), block_norm='L2-Hys',
+                       transform_sqrt=True,
+                       visualise=vis, feature_vector=feature_vec)
+    return features
+
+
+def convert_color(img, conversion='RGB2YCrCb'):
+    """
+    convert_color function converts and image per specified color convention.
+    :param img: image to convert
+    :param conversion: color space to convert
+    :return: converted image
+    """
+    if conversion == 'RGB2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+    if conversion == 'BGR2YCrCb':
+        return cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+    if conversion == 'RGB2LUV':
+        return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+    return img
+
+
+def bin_spatial(img, size=(32, 32)):
+    """
+    TODO: deliver description of this function
+    :param img:
+    :param size:
+    :return:
+    """
+    color1 = cv2.resize(img[:, :, 0], size).ravel()
+    color2 = cv2.resize(img[:, :, 1], size).ravel()
+    color3 = cv2.resize(img[:, :, 2], size).ravel()
+    return np.hstack((color1, color2, color3))
+
+
+def color_hist(img, nbins=32, bins_range=(0, 256)):
+    """
+    TODO: deliver description of this function
+    :param img:
+    :param nbins:
+    :param bins_range:
+    :return:
+    """
+    # Compute the histogram of the color channels separately
+    channel1_hist = np.histogram(img[:, :, 0], bins=nbins, range=bins_range)
+    channel2_hist = np.histogram(img[:, :, 1], bins=nbins, range=bins_range)
+    channel3_hist = np.histogram(img[:, :, 2], bins=nbins, range=bins_range)
+    # Concatenate the histograms into a single feature vector
+    bin_edges = channel1_hist[1]
+    bin_centers = (bin_edges[1:]  + bin_edges[0:len(bin_edges)-1])/2
+    hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
+    # Return the individual histograms, bin_centers and feature vector
+    return hist_features
+
+
+def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
+                     hist_bins=32, orient=9,
+                     pix_per_cell=8, cell_per_block=2, hog_channel=0,
+                     spatial_feat=True, hist_feat=True, hog_feat=True):
+    """
+    This function extracts features from a list of images
+    Have this function call bin_spatial() and color_hist()
+    :param imgs:
+    :param color_space: color space to use
+    :param spatial_size:
+    :param hist_bins: number of bins in histogram
+    :param orient: number of orientation bins
+    :param pix_per_cell: size in pixels of a cell
+    :param cell_per_block: number of cells in each block
+    :param hog_channel: HOG channel to use
+    :param spatial_feat:
+    :param hist_feat:
+    :param hog_feat:
+    :return:
+    """
+    # Create a list to append feature vectors to
+    features = []
+    # Iterate through the list of images
+    for file in imgs:
+        file_features = []
+        # Read in each one by one
+        image = mpimg.imread(file)
+        # apply color conversion if other than 'RGB'
+        if color_space != 'RGB':
+            if color_space == 'HSV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+            elif color_space == 'LUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+            elif color_space == 'HLS':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+            elif color_space == 'YUV':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+            elif color_space == 'YCrCb':
+                feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+        else:
+            feature_image = np.copy(image)
+
+        if spatial_feat is True:
+            spatial_features = bin_spatial(feature_image, size=spatial_size)
+            file_features.append(spatial_features)
+        if hist_feat is True:
+            # Apply color_hist()
+            hist_features = color_hist(feature_image, nbins=hist_bins)
+            file_features.append(hist_features)
+        if hog_feat is True:
+            # Call get_hog_features() with vis=False, feature_vec=True
+            if hog_channel == 'ALL':
+                hog_features = []
+                for channel in range(feature_image.shape[2]):
+                    hog_features.append(get_hog_features(feature_image[:, :, channel],
+                                                         orient, pix_per_cell, cell_per_block,
+                                                         vis=False, feature_vec=True))
+                hog_features = np.ravel(hog_features)
+            else:
+                hog_features = get_hog_features(feature_image[:, :, hog_channel], orient,
+                                                pix_per_cell, cell_per_block, vis=False, feature_vec=True)
+            # Append the new feature vector to the features list
+            file_features.append(hog_features)
+        features.append(np.concatenate(file_features))
+    # Return list of feature vectors
+    return features
+
+
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                 xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    """
+    This function takes an image calcualtes sliding widndows with the following paremeters/conditions:
+    - start and stop positions in both x and y,
+    - window size specified by xy_window,
+    - and overlap fraction (for both x and y)
+
+    :param img:
+    :param x_start_stop: starting x coordinate
+    :param y_start_stop: starting y coordinate
+    :param xy_window: size/shape of the sliding window
+    :param xy_overlap: overlap when sliding a window over an image
+    :return: list of sliding windows
+    """
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] is None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] is None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] is None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] is None:
+        y_start_stop[1] = img.shape[0]
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0] * (1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1] * (1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_buffer = np.int(xy_window[0] * (xy_overlap[0]))
+    ny_buffer = np.int(xy_window[1] * (xy_overlap[1]))
+    nx_windows = np.int((xspan - nx_buffer) / nx_pix_per_step)
+    ny_windows = np.int((yspan - ny_buffer) / ny_pix_per_step)
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    # Note: you could vectorize this step, but in practice
+    # you'll be considering windows one by one with your
+    # classifier, so looping makes sense
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs * nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys * ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
 
 
 def main_histogram():
