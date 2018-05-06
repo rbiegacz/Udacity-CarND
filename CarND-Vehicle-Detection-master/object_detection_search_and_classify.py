9bdf30d4b0926d47d131511dddc0e1fae18df8b0
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-05-06 14:00:28 +0200

    processin of many files implemented via "main_search_and_classify_many"

--- CarND-Vehicle-Detection-master/object_detection_search_and_classify.py
+++ CarND-Vehicle-Detection-master/object_detection_search_and_classify.py
@@ -52,7 +52,8 @@ def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
         elif color_space == 'YCrCb':
             feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
     else:
-        feature_image = np.copy(img)
+        #feature_image = np.copy(img)
+        feature_image = img
     # 3) Compute spatial features if flag is set
     if spatial_feat is True:
         spatial_features = object_detection_utils.bin_spatial(feature_image, size=spatial_size)
@@ -137,147 +138,113 @@ def search_windows(img, windows, clf, scaler, color_space='RGB',
     return on_windows
 
 
+def main_search_and_classify_many(model=None, image_files=None, use_heatmap=False,
+                                  display_results=False, method="both"):
+    images = []
+    k = 1
+    fig, axs = plt.subplots(3, 2, figsize=(20, 40))
+    axs = axs.ravel()
+    for image_file in image_files:
+        img = main_search_and_classify(model, None, image_file=image_file,
+                                       use_heatmap=use_heatmap, display_results=False,
+                                       method=method)
+
+        img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if use_heatmap:
+            cv2.imwrite("output_images/heatmap_processed_{}".format(image_file.split('/')[-1]), 255*img_RGB)
+        else:
+            cv2.imwrite("output_images/processed_{}".format(image_file.split('/')[-1]), 255*img_RGB)
+        plt.title('Image {}'.format(k))
+        axs[k-1].axis('off')
+        axs[k-1].set_title('Image {}'.format(k), fontsize=10)
+        axs[k-1].imshow(img)
+        k += 1
+
+
 def main_search_and_classify(model=None, image=None, image_file='util_images/bbox-example-image.png',
-                             use_heatmap=False, display_results=True):
+                             use_heatmap=False, display_results=True, method="both"):
     """
     TODO: write a description of this function
     :return:
     """
     # Read in cars and notcars
     # images = glob.glob('test_images//*.jpeg')
-    if model is None:
-        cars = glob.glob('util_images/vehicles/**/*.png')
-        notcars = glob.glob('util_images/non-vehicles/**/*.png')
-
-        # Reduce the sample size because
-        # The quiz evaluator times out after 13s of CPU time
-        sample_size = 1000
-        cars = cars[0:sample_size]
-        notcars = notcars[0:sample_size]
-
-        ### TODO: Tweak these parameters and see how the results change.
-        color_space = 'YUV'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-        orient = 9  # HOG orientations
-        pix_per_cell = 16  # HOG pixels per cell
-        cell_per_block = 2  # HOG cells per block
-        hog_channel = "ALL"  # Can be 0, 1, 2, or "ALL"
-        spatial_size = (16, 16)  # Spatial binning dimensions
-        hist_bins = 32  # Number of histogram bins
-        spatial_feat = True  # Spatial features on or off
-        hist_feat = True  # Histogram features on or off
-        hog_feat = True  # HOG features on or off
-
-        car_features = object_detection_utils.extract_features(cars, color_space=color_space,\
-                                                               spatial_size=spatial_size, hist_bins=hist_bins,
-                                                               orient=orient, pix_per_cell=pix_per_cell,
-                                                               cell_per_block=cell_per_block,
-                                                               hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                                               hist_feat=hist_feat, hog_feat=hog_feat)
-        notcar_features = \
-            object_detection_utils.extract_features(notcars, color_space=color_space,
-                                                    spatial_size=spatial_size, hist_bins=hist_bins,
-                                                    orient=orient, pix_per_cell=pix_per_cell,
-                                                    cell_per_block=cell_per_block,
-                                                    hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                                    hist_feat=hist_feat, hog_feat=hog_feat)
-
-        # Create an array stack of feature vectors
-        X = np.vstack((car_features, notcar_features)).astype(np.float64)
-
-        # Define the labels vector
-        y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
-
-        # Split up data into randomized training and test sets
-        rand_state = np.random.randint(0, 100)
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=rand_state)
-
-        # Fit a per-column scaler
-        X_scaler = StandardScaler().fit(X_train)
-        # Apply the scaler to X
-        X_train = X_scaler.transform(X_train)
-        X_test = X_scaler.transform(X_test)
-
-        print('Using:', orient, 'orientations', pix_per_cell,
-              'pixels per cell and', cell_per_block, 'cells per block')
-        print('Feature vector length:', len(X_train[0]))
-        # Use a linear SVC
-        svc = LinearSVC()
-        # Check the training time for the SVC
-        t = time.time()
-        svc.fit(X_train, y_train)
-        t2 = time.time()
-        print(round(t2 - t, 2), 'Seconds to train SVC...')
-        # Check the score of the SVC
-        print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-        # Check the prediction time for a single sample
-        t = time.time()
-    else:
-        svc = model["linearSVC"]
-        X_scaler = model["X_scaler"]
-        color_space = model["color_item"]
-        orient = model["orient"]
-        pix_per_cell = model["pix_per_cell"]
-        cell_per_block = model["cells_per_block"]
-        spatial_size = (32, 32)  # Spatial binning dimensions
-        hist_bins = 2*pix_per_cell  # Number of histogram bins
-        hog_channel = model["hog_channel"]
-        spatial_feat = model["spatial_feat"]  # Spatial features on or off
-        hist_feat = model["hist_feat"]  # Histogram features on or off
-        hog_feat = model["hog_feat"]  # HOG features on or off
+    svc = model["linearSVC"]
+    X_scaler = model["X_scaler"]
+    color_space = model["color_item"]
+    orient = model["orient"]
+    pix_per_cell = model["pix_per_cell"]
+    cell_per_block = model["cells_per_block"]
+    spatial_size = (32, 32)  # Spatial binning dimensions
+    hist_bins = 2*pix_per_cell  # Number of histogram bins
+    hog_channel = model["hog_channel"]
+    spatial_feat = model["spatial_feat"]  # Spatial features on or off
+    hist_feat = model["hist_feat"]  # Histogram features on or off
+    hog_feat = model["hog_feat"]  # HOG features on or off
 
     if image_file is not None:
         image = mpimg.imread(image_file)
-    draw_image = np.copy(image)
+    # draw_image = np.copy(image)
+    draw_image = image
     # Uncomment the following line if you extracted training
     # data from .png images (scaled 0 to 1 by mpimg) and the
     # image you are searching is a jpg (scaled 0 to 255)
     # image = image.astype(np.float32)/255
 
-    windows_list = []
-    y_start_stop_list = [(300, 500), (450, 600), (600, 700)]
-    xy_window_list = [(64, 64), (64, 64), (64, 64)]
-    overlap_list = [(0.3, 0.3), (0.3, 0.3), (0.3, 0.3)]
+    hotty_windows = []
 
-    y_start_stop_list = [(400, 650)]
-    xy_window_list = [(128, 128)]
-    overlap_list = [(0.85, 0.85)]
+    if method in ["both", "sliding_windows"]:
+        windows_list = []
+        x_start_stop_list = [[0, 400], [410, 850], [860, None]]
+        y_start_stop_list = [(450, 70), (420, 500), (450, 700)]
+        xy_window_list = [(128, 128), (64, 64), (128, 128)]
 
-    for y in y_start_stop_list:
-        for xy, xy_overlap in zip(xy_window_list, overlap_list):
-            windows_list += (object_detection_utils.slide_window(image, x_start_stop=[None, None],
+        x_start_stop_list = [[50, 1280], [50, 1280], [860, 1200]]
+        y_start_stop_list = [(410, 450), (400, 600), (500, 700)]
+        xy_window_list = [(32, 32), (64, 64), (128, 128)]
+        overlapx = 0.75
+        overlapy = 0.75
+        overlap_list = [(overlapx, overlapy), (overlapx, overlapy), (overlapx, overlapy)]
+
+        for x, y, xy, xy_overlap in zip(x_start_stop_list, y_start_stop_list, xy_window_list, overlap_list):
+            windows_list += (object_detection_utils.slide_window(image, x_start_stop=x,
                                                                  y_start_stop=y, xy_window=xy,
                                                                  xy_overlap=xy_overlap))
-    hot_windows = search_windows(image, windows_list, svc, X_scaler, color_space=color_space,
-                                 spatial_size=spatial_size, hist_bins=hist_bins,
-                                 orient=orient, pix_per_cell=pix_per_cell,
-                                 cell_per_block=cell_per_block,
-                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                 hist_feat=hist_feat, hog_feat=hog_feat)
+        hot_windows = search_windows(image, windows_list, svc, X_scaler, color_space=color_space,
+                                     spatial_size=spatial_size, hist_bins=hist_bins,
+                                     orient=orient, pix_per_cell=pix_per_cell,
+                                     cell_per_block=cell_per_block,
+                                     hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                     hist_feat=hist_feat, hog_feat=hog_feat)
+        hotty_windows = hot_windows
 
-    ystart_list = [400, 416, 400, 432, 400, 432, 400, 464]
-    ystop_list = [464, 480, 496, 528, 528, 560, 596, 660]
-    scale_list = [2.0, 2.0, 1.5, 1.5, 2.0, 2.0, 3.5, 3.5]
-    rectangles = []
+    if method in ["both", "hot_windows"]:
+        xstart_list = [200, 200, 1000, 400, 400]
+        xstop_list = [1280, 900, 1280, 800, 1280]
+        ystart_list = [420, 420, 450, 450, 500]
+        ystop_list = [600, 650, 600, 600, 720]
+        scale_list = [1.5, 2.0, 1, 2, 3]
+        rectangles = []
+        for ystart, ystop, scale, xstart, xstop in zip(ystart_list, ystop_list, scale_list, xstart_list, xstop_list):
+            rectangles += \
+                object_detection_hog_subsampling.find_cars(image, ystart, ystop, scale, color_space,
+                                                           hog_channel, svc, X_scaler, orient, pix_per_cell,
+                                                           cell_per_block,spatial_size, hist_bins, xstart, xstop)
+        hotty_windows += rectangles
 
-    for ystart, ystop, scale in zip(ystart_list, ystop_list, scale_list):
-        rectangles += \
-            object_detection_hog_subsampling.find_cars(image, ystart, ystop, scale, color_space,
-                                                       hog_channel, svc, X_scaler, orient, pix_per_cell,
-                                                       cell_per_block,spatial_size, hist_bins)
-
-    hotty_windows = hot_windows
     if use_heatmap:
         heat = np.zeros_like(image[:, :, 0]).astype(np.float)
         # Add heat to each box in box list
         heat = object_detection_heatmap.add_heat(heat, hotty_windows)
         # Apply threshold to help remove false positives
-        heat = object_detection_heatmap.apply_threshold(heat, 3)
+        heat = object_detection_heatmap.apply_threshold(heat, 4)
         # Visualize the heatmap when displaying
-        heatmap = np.clip(heat, 0, 255)
+        #heatmap = np.clip(heat, 0, 255)
+        heatmap = np.clip(heat, 0, 1)
         # Find final boxes from heatmap using label function
         labels = object_detection_heatmap.label(heatmap)
-        draw_img = object_detection_heatmap.draw_labeled_bboxes(np.copy(image), labels)
+        #draw_img = object_detection_heatmap.draw_labeled_bboxes(np.copy(image), labels)
+        draw_img = object_detection_heatmap.draw_labeled_bboxes(image, labels)
         if display_results:
             fig = plt.figure()
             plt.subplot(121)
@@ -287,20 +254,13 @@ def main_search_and_classify(model=None, image=None, image_file='util_images/bbo
             plt.imshow(heatmap, cmap='hot')
             plt.title('Heat Map')
             fig.tight_layout()
-
-
-    if not use_heatmap:
-        draw_img = object_detection_utils.draw_boxes(draw_image, hotty_windows, color=(0, 0, 255), thick=6)
-        window_img1 = object_detection_utils.draw_boxes(draw_image, windows_list, color=(0, 0, 255), thick=6)
+    else:
+        draw_img = object_detection_utils.draw_boxes(image, hotty_windows, color=(0, 0, 255), thick=6)
         if display_results:
             fig = plt.figure()
-            plt.subplot(121)
+            plt.subplot(211)
             plt.imshow(draw_img)
-            plt.title('Name it')
-            plt.subplot(122)
-            plt.imshow(window_img1)
-            plt.title('Name it')
-            fig.tight_layout()
+            plt.title('Identified objects')
     return draw_img
 
 if __name__ == '__main__':
