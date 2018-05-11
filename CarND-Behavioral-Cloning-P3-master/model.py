1ec95f52b6321bfc18e2b2184c51da0fdaef81e9
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-05-11 02:05:12 +0200

    train_model_2 implemented (uses generators)

--- CarND-Behavioral-Cloning-P3-master/model.py
+++ CarND-Behavioral-Cloning-P3-master/model.py
@@ -22,12 +22,13 @@ import os
 import csv
 import cv2
 import numpy as np
+from random import shuffle
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
 from keras.layers.convolutional import Convolution2D, Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.models import Model
-
+import sklearn
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
@@ -38,7 +39,44 @@ RESIZE_SCALE = 4
 y_size = 160
 x_size = 320
 
-def load_data():
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            correction = 0.1
+            for batch_sample in batch_samples:
+                name = 'data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_image = cv2.cvtColor(center_image, cv2.COLOR_BGR2RGB)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+                name = 'data/IMG/'+batch_sample[1].split('/')[-1]
+                left_image = cv2.imread(name)
+                left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
+                left_angle = float(batch_sample[3]) + correction
+                images.append(left_image)
+                angles.append(left_angle)
+
+                name = 'data/IMG/'+batch_sample[1].split('/')[-1]
+                right_image = cv2.imread(name)
+                right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
+                right_angle = float(batch_sample[3]) + correction
+                images.append(right_image)
+                angles.append(right_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+def load_data(read_only_data=False):
     """
     this function is responsible for loading data and augmenting dataset
     :return:
@@ -47,7 +85,6 @@ def load_data():
     with open('data/driving_log.csv') as csvfile:
         reader = csv.reader(csvfile)
         for line in reader:
-            #steering_center = float(line[3])
             lines.append(line)
     if len(lines) == 0:
         print("I cannot read data!")
@@ -55,6 +92,9 @@ def load_data():
     measurements = []
     aug_images = []
     aug_measurements = []
+
+    if read_only_data:
+        return lines[1:]
 
     for line in lines[1:]:
         source_path = line[0]
@@ -146,36 +186,13 @@ def train_model():
     else:
         model.save('lenet_model.h5')
 
-def generator(samples, batch_size=32):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
-        for offset in range(0, num_samples, batch_size):
-            batch_samples = samples[offset:offset+batch_size]
-
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
-
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
-
 def train_model_2():
     """
     this function is responsible for training the model
     :return:
     """
-    aug_images, aug_measurements = load_data()
-
-    X_train = np.array(aug_images)
-    y_train = np.array(aug_measurements)
+    samples = load_data(read_only_data=True)
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
     simple_model = False
 
@@ -217,4 +234,4 @@ def train_model_2():
         model.save('lenet_model.h5')
 
 if __name__ == '__main__':
-    train_model()
+    train_model_2()
