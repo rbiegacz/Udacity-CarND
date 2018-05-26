16c65aaf986b70cb275ad94b3ea3eee288a7495c
Rafal Biegacz <biegacz.rafal@gmail.com>
2018-05-26 02:42:02 +0200

    removing needless code

--- CarND-Advanced-Lane-Lines-master/searchlines.py
+++ CarND-Advanced-Lane-Lines-master/searchlines.py
@@ -136,9 +136,6 @@ def search_for_lines(file_image, img=None):
 
     # Take a histogram of the bottom half of the image
     histogram = np.sum(binary_warped[int(binary_warped.shape[0]/2):, :], axis=0)
-    # Create an output image to draw on and  visualize the result
-    if False:
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
     # Find the peak of the left and right halves of the histogram
     # These will be the starting point for the left and right lines
     midpoint = np.int(histogram.shape[0]/2)
@@ -173,12 +170,6 @@ def search_for_lines(file_image, img=None):
         win_xleft_high = int(leftx_current + margin)
         win_xright_low = int(rightx_current - margin)
         win_xright_high = int(rightx_current + margin)
-        # Draw the windows on the visualization image
-        if False:
-            line_color1 = (0, 255, 0)
-            line_color2 = (0, 0, 255)
-            cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), line_color1, 2)
-            cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), line_color2, 2)
         # Identify the nonzero pixels in x and y within the window
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
                           (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
@@ -216,10 +207,6 @@ def search_for_lines(file_image, img=None):
     left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
 
-    if False:
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-
     nonzero = binary_warped.nonzero()
     nonzeroy = np.array(nonzero[0])
     nonzerox = np.array(nonzero[1])
@@ -244,39 +231,13 @@ def search_for_lines(file_image, img=None):
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
     # Generate x and y values for plotting
-    how_long_lane = 2
-    ystart = 1
+    how_long_lane = 1
+    ystart = 0
     ploty = np.linspace(ystart,
                         (binary_warped.shape[0]-1-ystart)//how_long_lane,
                         (binary_warped.shape[0]-ystart)//how_long_lane)
     left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
-    if False:
-        # And you're done! But let's visualize the result here as well
-        # Create an image to draw on and an image to show the selection window
-        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
-        window_img = np.zeros_like(out_img)
-        # Color in left and right line pixels
-        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-
-        # Generate a polygon to illustrate the search window area
-        # And recast the x and y points into usable format for cv2.fillPoly()
-        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
-        left_line_window2 = \
-            np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, ploty])))])
-        left_line_pts = np.hstack((left_line_window1, left_line_window2))
-        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
-        right_line_window2 = \
-            np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, ploty])))])
-        right_line_pts = np.hstack((right_line_window1, right_line_window2))
-
-    # Draw the lane onto the warped blank image
-    if False:
-        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
-        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 0, 255))
-        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
 
     output = dict()
     output['left_fit'] = left_fit
@@ -286,13 +247,9 @@ def search_for_lines(file_image, img=None):
     output['ploty'] = ploty
     output['nonzerox'] = nonzerox
     output['nonzeroy'] = nonzeroy
-    if False:
-        output['out_img'] = result
-    else:
-        result = None
     output['left_lane_inds'] = left_lane_inds
     output['right_lane_inds'] = right_lane_inds
-    return result, output
+    return None, output
 
 
 def main():
